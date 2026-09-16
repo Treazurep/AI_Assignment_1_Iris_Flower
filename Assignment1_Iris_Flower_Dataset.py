74c466679c748a1eace93c84786b5ee652d2a834
Treazurep <treazurep89@gmail.com>
2026-09-16 10:17:07 -0500

    Task 2 Load a Real CSV Dataset From the Internet

--- Assignment1_Iris_Flower_Dataset.py
+++ Assignment1_Iris_Flower_Dataset.py
@@ -2,4 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-print(pd.__version__)
+
+url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+ 
+data = pd.read_csv(url)
+
+# display the first 5 rows of the dataset
+print(data.head(5))
