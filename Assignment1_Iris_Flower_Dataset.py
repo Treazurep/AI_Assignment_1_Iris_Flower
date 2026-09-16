a3641fbe7bbd146e22ee328de78a83169a972d28
Treazurep <treazurep89@gmail.com>
2026-09-16 10:34:56 -0500

    Task 3 Explore the Dataset

--- Assignment1_Iris_Flower_Dataset.py
+++ Assignment1_Iris_Flower_Dataset.py
@@ -6,6 +6,13 @@ import seaborn as sns
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
  
 data = pd.read_csv(url)
-
 # display the first 5 rows of the dataset
-print(data.head(5))
+# print(data.head(5))
+
+# display the first 10 rows of the dataset
+print(data.head(10))
+
+data.shape
+data.columns
+data.info()
+data.describe()
